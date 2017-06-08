25d60f1d19cc6247745fe267af126260a35c3978
Frederic Alexandre Dreyer <frederic.dreyer@cern.ch>
2017-06-08 23:14:22 +0200

    added a bunch of ECF based observables to the SubstructureVariables class

--- analyses/SubstructureVariables.cc
+++ analyses/SubstructureVariables.cc
@@ -42,8 +42,15 @@ SubstructureVariables::SubstructureVariables(CmdLine &cmdline){
   _tau2_beta1.reset(new Nsubjettiness(2, axes_kt,  measure1));
   _tau2_beta2.reset(new Nsubjettiness(2, axes_gkt, measure2));
   
+  _1ecf2_beta1.reset(new EnergyCorrelatorGeneralized(1, 2, 1.0));
+  _1ecf3_beta1.reset(new EnergyCorrelatorGeneralized(1, 3, 1.0));
+  _2ecf3_beta1.reset(new EnergyCorrelatorGeneralized(2, 3, 1.0));
+  _3ecf3_beta1.reset(new EnergyCorrelatorGeneralized(3, 3, 1.0));
 
-  //TODO: EnergyCorrelator _ecf1_beta1, _ecf2_beta1, _ecf3_beta1, _ecf1_beta2, _ecf2_beta2, _ecf3_beta2;
+  _1ecf2_beta2.reset(new EnergyCorrelatorGeneralized(1, 2, 2.0));
+  _1ecf3_beta2.reset(new EnergyCorrelatorGeneralized(1, 3, 2.0));
+  _2ecf3_beta2.reset(new EnergyCorrelatorGeneralized(2, 3, 2.0));
+  _3ecf3_beta2.reset(new EnergyCorrelatorGeneralized(3, 3, 2.0));
 
   // additional tools:
   // mMDT andSD will need a C/A reclustering
@@ -65,10 +72,27 @@ void SubstructureVariables::set_jet(const PseudoJet &jet){
   PseudoJet mmdt_jet    = (*_mmdt)(ca_jet);
 
   // all quantities for the plain jet
-  _compute_one_level(jet,      _m_plain, _tau1_beta1_plain, _tau2_beta1_plain, _tau1_beta2_plain, _tau2_beta2_plain);
-  _compute_one_level(sd_jet,   _m_loose, _tau1_beta1_loose, _tau2_beta1_loose, _tau1_beta2_loose, _tau2_beta2_loose);
-  _compute_one_level(mmdt_jet, _m_tight, _tau1_beta1_tight, _tau2_beta1_tight, _tau1_beta2_tight, _tau2_beta2_tight);
-  //TODO: add ecfs
+  _compute_one_level(jet,      _m_plain,
+		     _tau1_beta1_plain, _tau2_beta1_plain,
+		     _tau1_beta2_plain, _tau2_beta2_plain,
+		     _1ecf2_beta1_plain, _1ecf3_beta1_plain,
+		     _2ecf3_beta1_plain, _3ecf3_beta1_plain,
+		     _1ecf2_beta2_plain, _1ecf3_beta2_plain,
+		     _2ecf3_beta2_plain, _3ecf3_beta2_plain);
+  _compute_one_level(sd_jet,   _m_loose,
+		     _tau1_beta1_loose, _tau2_beta1_loose,
+		     _tau1_beta2_loose, _tau2_beta2_loose,
+		     _1ecf2_beta1_loose, _1ecf3_beta1_loose,
+		     _2ecf3_beta1_loose, _3ecf3_beta1_loose,
+		     _1ecf2_beta2_loose, _1ecf3_beta2_loose,
+		     _2ecf3_beta2_loose, _3ecf3_beta2_loose);
+  _compute_one_level(mmdt_jet, _m_tight,
+		     _tau1_beta1_tight, _tau2_beta1_tight,
+		     _tau1_beta2_tight, _tau2_beta2_tight,
+		     _1ecf2_beta1_tight, _1ecf3_beta1_tight,
+		     _2ecf3_beta1_tight, _3ecf3_beta1_tight,
+		     _1ecf2_beta2_tight, _1ecf3_beta2_tight,
+		     _2ecf3_beta2_tight, _3ecf3_beta2_tight);
 
   // trimmed quantities are sepatare
   _m_trim  = trimmed_jet.m();
@@ -78,12 +102,24 @@ void SubstructureVariables::set_jet(const PseudoJet &jet){
 void SubstructureVariables::_compute_one_level(const PseudoJet &jet,
                                                double &m,
                                                double &tau1_beta1, double &tau2_beta1,
-                                               double &tau1_beta2, double &tau2_beta2){
-  //TODO: add ecfs
+                                               double &tau1_beta2, double &tau2_beta2,
+					       double &e12_beta1, double& e13_beta1,
+					       double &e23_beta1, double& e33_beta1,
+					       double &e12_beta2, double& e13_beta2,
+					       double &e23_beta2, double& e33_beta2){
   m = jet.m();
   tau1_beta1 = (*_tau1_beta1)(jet);
   tau2_beta1 = (*_tau2_beta1)(jet);
   tau1_beta2 = (*_tau1_beta2)(jet);
   tau2_beta2 = (*_tau2_beta2)(jet);
+  // ecfgs
+  e12_beta1 = (*_1ecf2_beta1)(jet);
+  e13_beta1 = (*_1ecf3_beta1)(jet);
+  e23_beta1 = (*_2ecf3_beta1)(jet);
+  e33_beta1 = (*_3ecf3_beta1)(jet);
+  e12_beta2 = (*_1ecf2_beta2)(jet);
+  e13_beta2 = (*_1ecf3_beta2)(jet);
+  e23_beta2 = (*_2ecf3_beta2)(jet);
+  e33_beta2 = (*_3ecf3_beta2)(jet);
 }
 
--- analyses/SubstructureVariables.hh
+++ analyses/SubstructureVariables.hh
@@ -56,6 +56,8 @@ public:
   double tau21_beta1_plain() const { return _tau2_beta1_plain/_tau2_beta1_plain;}
   double tau21_beta1_loose() const { return _tau2_beta1_loose/_tau2_beta1_loose;}
   double tau21_beta1_tight() const { return _tau2_beta1_tight/_tau2_beta1_tight;}
+  double tau21_beta1_plain_loose() const { return _tau2_beta1_plain/_tau2_beta1_loose;}
+  double tau21_beta1_plain_tight() const { return _tau2_beta1_plain/_tau2_beta1_tight;}
   
   double tau1_beta2_plain () const { return _tau1_beta2_plain;}
   double tau1_beta2_loose () const { return _tau1_beta2_loose;}
@@ -66,19 +68,110 @@ public:
   double tau21_beta2_plain() const { return _tau2_beta2_plain/_tau2_beta2_plain;}
   double tau21_beta2_loose() const { return _tau2_beta2_loose/_tau2_beta2_loose;}
   double tau21_beta2_tight() const { return _tau2_beta2_tight/_tau2_beta2_tight;}
+  double tau21_beta2_plain_loose() const { return _tau2_beta2_plain/_tau2_beta2_loose;}
+  double tau21_beta2_plain_tight() const { return _tau2_beta2_plain/_tau2_beta2_tight;}
 
-  //TODO: add ecfs
-
+  // ecfs based observables
+  // N2 beta=1
+  double N2_beta1_plain   () const {
+    return _2ecf3_beta1_plain / (_1ecf2_beta1_plain*_1ecf2_beta1_plain);}
+  double N2_beta1_loose   () const {
+    return _2ecf3_beta1_loose / (_1ecf2_beta1_loose*_1ecf2_beta1_loose);}
+  double N2_beta1_tight   () const {
+    return _2ecf3_beta1_tight / (_1ecf2_beta1_tight*_1ecf2_beta1_tight);}
+  double N2_beta1_plain_loose   () const {
+    return _2ecf3_beta1_plain / (_1ecf2_beta1_loose*_1ecf2_beta1_loose);}
+  double N2_beta1_plain_tight   () const {
+    return _2ecf3_beta1_plain / (_1ecf2_beta1_tight*_1ecf2_beta1_tight);}
+  // M2 beta=1
+  double M2_beta1_plain   () const {
+    return _1ecf3_beta1_plain / _1ecf2_beta1_plain;}
+  double M2_beta1_loose   () const {
+    return _1ecf3_beta1_loose / _1ecf2_beta1_loose;}
+  double M2_beta1_tight   () const {
+    return _1ecf3_beta1_tight / _1ecf2_beta1_tight;}
+  double M2_beta1_plain_loose   () const {
+    return _1ecf3_beta1_plain / _1ecf2_beta1_loose;}
+  double M2_beta1_plain_tight   () const {
+    return _1ecf3_beta1_plain / _1ecf2_beta1_tight;}
+  // D2 beta=1
+  double D2_beta1_plain   () const {
+    return _3ecf3_beta1_plain
+      / (_1ecf2_beta1_plain*_1ecf2_beta1_plain*_1ecf2_beta1_plain);}
+  double D2_beta1_loose   () const {
+    return _3ecf3_beta1_loose
+      / (_1ecf2_beta1_loose*_1ecf2_beta1_loose*_1ecf2_beta1_loose);}
+  double D2_beta1_tight   () const {
+    return _3ecf3_beta1_tight
+      / (_1ecf2_beta1_tight*_1ecf2_beta1_tight*_1ecf2_beta1_tight);}
+  double D2_beta1_plain_loose   () const {
+    return _3ecf3_beta1_plain
+      / (_1ecf2_beta1_loose*_1ecf2_beta1_loose*_1ecf2_beta1_loose);}
+  double D2_beta1_plain_tight   () const {
+    return _3ecf3_beta1_plain
+      / (_1ecf2_beta1_tight*_1ecf2_beta1_tight*_1ecf2_beta1_tight);}
+  
+  // N2 beta=2
+  double N2_beta2_plain   () const {
+    return _2ecf3_beta2_plain / (_1ecf2_beta2_plain*_1ecf2_beta2_plain);}
+  double N2_beta2_loose   () const {
+    return _2ecf3_beta2_loose / (_1ecf2_beta2_loose*_1ecf2_beta2_loose);}
+  double N2_beta2_tight   () const {
+    return _2ecf3_beta2_tight / (_1ecf2_beta2_tight*_1ecf2_beta2_tight);}
+  double N2_beta2_plain_loose   () const {
+    return _2ecf3_beta2_plain / (_1ecf2_beta2_loose*_1ecf2_beta2_loose);}
+  double N2_beta2_plain_tight   () const {
+    return _2ecf3_beta2_plain / (_1ecf2_beta2_tight*_1ecf2_beta2_tight);}
+  // M2 beta=2
+  double M2_beta2_plain   () const {
+    return _1ecf3_beta2_plain / _1ecf2_beta2_plain;}
+  double M2_beta2_loose   () const {
+    return _1ecf3_beta2_loose / _1ecf2_beta2_loose;}
+  double M2_beta2_tight   () const {
+    return _1ecf3_beta2_tight / _1ecf2_beta2_tight;}
+  double M2_beta2_plain_loose   () const {
+    return _1ecf3_beta2_plain / _1ecf2_beta2_loose;}
+  double M2_beta2_plain_tight   () const {
+    return _1ecf3_beta2_plain / _1ecf2_beta2_tight;}
+  // D2 beta=2
+  double D2_beta2_plain   () const {
+    return _3ecf3_beta2_plain
+      / (_1ecf2_beta2_plain*_1ecf2_beta2_plain*_1ecf2_beta2_plain);}
+  double D2_beta2_loose   () const {
+    return _3ecf3_beta2_loose
+      / (_1ecf2_beta2_loose*_1ecf2_beta2_loose*_1ecf2_beta2_loose);}
+  double D2_beta2_tight   () const {
+    return _3ecf3_beta2_tight
+      / (_1ecf2_beta2_tight*_1ecf2_beta2_tight*_1ecf2_beta2_tight);}
+  double D2_beta2_plain_loose   () const {
+    return _3ecf3_beta2_plain
+      / (_1ecf2_beta2_loose*_1ecf2_beta2_loose*_1ecf2_beta2_loose);}
+  double D2_beta2_plain_tight   () const {
+    return _3ecf3_beta2_plain
+      / (_1ecf2_beta2_tight*_1ecf2_beta2_tight*_1ecf2_beta2_tight);}
+  
 protected:
   //----------------------------------------------------------------------
-  // cached results 
+  // cached results
   double _m_plain, _m_loose, _m_tight, _m_trim;                   ///< masses
+  
+  // N-subjettiness
   double _tau1_beta1_plain, _tau1_beta1_loose, _tau1_beta1_tight; ///< tau1(beta=1)
   double _tau2_beta1_plain, _tau2_beta1_loose, _tau2_beta1_tight; ///< tau2(beta=1)
   double _tau1_beta2_plain, _tau1_beta2_loose, _tau1_beta2_tight; ///< tau1(beta=2)
   double _tau2_beta2_plain, _tau2_beta2_loose, _tau2_beta2_tight; ///< tau2(beta=2)
-  //TODO: add ecfs
 
+  // ecfgs
+  double _1ecf2_beta1_plain, _1ecf2_beta1_loose, _1ecf2_beta1_tight; ///< 1ecfg2(beta=1)
+  double _1ecf3_beta1_plain, _1ecf3_beta1_loose, _1ecf3_beta1_tight; ///< 1ecfg3(beta=1)
+  double _2ecf3_beta1_plain, _2ecf3_beta1_loose, _2ecf3_beta1_tight; ///< 2ecfg3(beta=1)
+  double _3ecf3_beta1_plain, _3ecf3_beta1_loose, _3ecf3_beta1_tight; ///< 3ecfg3(beta=1)
+  
+  double _1ecf2_beta2_plain, _1ecf2_beta2_loose, _1ecf2_beta2_tight; ///< 1ecfg2(beta=2)
+  double _1ecf3_beta2_plain, _1ecf3_beta2_loose, _1ecf3_beta2_tight; ///< 1ecfg3(beta=2)
+  double _2ecf3_beta2_plain, _2ecf3_beta2_loose, _2ecf3_beta2_tight; ///< 2ecfg3(beta=2)
+  double _3ecf3_beta2_plain, _3ecf3_beta2_loose, _3ecf3_beta2_tight; ///< 3ecfg3(beta=2)
+  
   //----------------------------------------------------------------------
   // substructure tools
   
@@ -88,8 +181,11 @@ protected:
   fastjet::SharedPtr<fastjet::contrib::SoftDrop> _sd;
 
   /// shape tools
-  fastjet::SharedPtr<fastjet::contrib::Nsubjettiness> _tau1_beta1, _tau1_beta2, _tau2_beta1, _tau2_beta2;
-  fastjet::SharedPtr<fastjet::contrib::EnergyCorrelator> _ecf1_beta1, _ecf2_beta1, _ecf3_beta1, _ecf1_beta2, _ecf2_beta2, _ecf3_beta2;
+  fastjet::SharedPtr<fastjet::contrib::Nsubjettiness> _tau1_beta1, _tau1_beta2,
+    _tau2_beta1, _tau2_beta2;
+  fastjet::SharedPtr<fastjet::contrib::EnergyCorrelatorGeneralized>
+    _1ecf2_beta1, _1ecf3_beta1, _2ecf3_beta1, _3ecf3_beta1,
+    _1ecf2_beta2, _1ecf3_beta2, _2ecf3_beta2, _3ecf3_beta2;
 
   /// underlying needs
   fastjet::SharedPtr<fastjet::Recluster> _recluster_ca;
@@ -99,7 +195,11 @@ protected:
   void _compute_one_level(const fastjet::PseudoJet &jet,
                           double &m,
                           double &tau1_beta1, double &tau2_beta1,
-                          double &tau1_beta2, double &tau2_beta2);
+                          double &tau1_beta2, double &tau2_beta2,
+			  double &e12_beta1, double& e13_beta1,
+			  double &e23_beta1, double& e33_beta1,
+			  double &e12_beta2, double& e13_beta2,
+			  double &e23_beta2, double& e33_beta2);
   //TODO: add ecfs
 
   std::ostringstream _description;
