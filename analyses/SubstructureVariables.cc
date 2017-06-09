8cda5c50312d1c193bfaffdf59f73fc70308e1f0
Frederic Alexandre Dreyer <frederic.dreyer@cern.ch>
2017-06-09 11:16:08 +0200

    added all beta values with a simpler format

--- analyses/SubstructureVariables.cc
+++ analyses/SubstructureVariables.cc
@@ -10,7 +10,7 @@ using namespace fastjet::contrib;
 //------------------------------------------------------------------------
 
 // ctor (initialises all tools)
-SubstructureVariables::SubstructureVariables(CmdLine &cmdline){
+SubstructureVariables::SubstructureVariables(CmdLine &cmdline) {
   _description << "# Substructure variables configuration" << endl;
 
   // trimming
@@ -33,17 +33,22 @@ SubstructureVariables::SubstructureVariables(CmdLine &cmdline){
   _description << "# SoftDrop: " << _sd->description() << endl;
 
   /// shape tools
-  double shape_beta = cmdline.value("-shape.beta", 1.0);
   contrib::WTA_KT_Axes  axes_kt;
   contrib::GenKT_Axes   axes_gkt(0.5);
   contrib::UnnormalizedMeasure measure1(1);
   contrib::UnnormalizedMeasure measure2(2);
-  _tau1.reset(new Nsubjettiness(shape_beta, axes_kt,  measure1));
-  _tau1.reset(new Nsubjettiness(shape_beta, axes_gkt, measure2));
-  _1ecf2.reset(new EnergyCorrelatorGeneralized(1, 2, shape_beta));
-  _1ecf3.reset(new EnergyCorrelatorGeneralized(1, 3, shape_beta));
-  _2ecf3.reset(new EnergyCorrelatorGeneralized(2, 3, shape_beta));
-  _3ecf3.reset(new EnergyCorrelatorGeneralized(3, 3, shape_beta));
+  _tau1[beta1].reset(new Nsubjettiness(1, axes_kt,  measure1));
+  _tau1[beta2].reset(new Nsubjettiness(2, axes_kt,  measure1));
+  _tau2[beta1].reset(new Nsubjettiness(1, axes_gkt, measure2));
+  _tau2[beta2].reset(new Nsubjettiness(2, axes_gkt, measure2));
+  _1ecf2[beta1].reset(new EnergyCorrelatorGeneralized(1, 2, 1));
+  _1ecf3[beta1].reset(new EnergyCorrelatorGeneralized(1, 3, 1));
+  _2ecf3[beta1].reset(new EnergyCorrelatorGeneralized(2, 3, 1));
+  _3ecf3[beta1].reset(new EnergyCorrelatorGeneralized(3, 3, 1));
+  _1ecf2[beta2].reset(new EnergyCorrelatorGeneralized(1, 2, 2));
+  _1ecf3[beta2].reset(new EnergyCorrelatorGeneralized(1, 3, 2));
+  _2ecf3[beta2].reset(new EnergyCorrelatorGeneralized(2, 3, 2));
+  _3ecf3[beta2].reset(new EnergyCorrelatorGeneralized(3, 3, 2));
 
   // additional tools:
   // mMDT andSD will need a C/A reclustering
@@ -65,36 +70,43 @@ void SubstructureVariables::set_jet(const PseudoJet &jet){
   PseudoJet mmdt_jet    = (*_mmdt)(ca_jet);
 
   // all quantities for the plain jet
-  _compute_one_level(jet,      _m_vals[plain],
-		     _tau1_vals[plain], _tau2_vals[plain],
+  _compute_one_level(jet, _tau1_vals[plain], _tau2_vals[plain],
 		     _1ecf2_vals[plain], _1ecf3_vals[plain],
 		     _2ecf3_vals[plain], _3ecf3_vals[plain]);
-  _compute_one_level(sd_jet,   _m_vals[loose],
-		     _tau1_vals[loose], _tau2_vals[loose],
+  _compute_one_level(sd_jet, _tau1_vals[loose], _tau2_vals[loose],
 		     _1ecf2_vals[loose], _1ecf3_vals[loose],
 		     _2ecf3_vals[loose], _3ecf3_vals[loose]);
-  _compute_one_level(mmdt_jet, _m_vals[tight],
-		     _tau1_vals[tight], _tau2_vals[tight],
+  _compute_one_level(mmdt_jet, _tau1_vals[tight], _tau2_vals[tight],
 		     _1ecf2_vals[tight], _1ecf3_vals[tight],
 		     _2ecf3_vals[tight], _3ecf3_vals[tight]);
 
-  // trimmed quantities are sepatare
+  // mass quantities are sepatare
+  _m_vals[plain] = jet.m();
+  _m_vals[loose] = sd_jet.m();
+  _m_vals[tight] = mmdt_jet.m();
   _m_vals[trim]  = trimmed_jet.m();
 
 }
 
 void SubstructureVariables::_compute_one_level(const PseudoJet &jet,
-                                               double &m,
-                                               double &tau1, double &tau2,
-					       double &e12, double& e13,
-					       double &e23, double& e33){
-  m = jet.m();
-  tau1 = (*_tau1)(jet);
-  tau2 = (*_tau2)(jet);
+                                               double (&tau1)[2], double (&tau2)[2],
+					       double (&e12)[2], double (&e13)[2],
+					       double (&e23)[2], double (&e33)[2]){
+  tau1[beta1] = (*(_tau1[beta1]))(jet);
+  tau2[beta1] = (*(_tau2[beta1]))(jet);
+  
+  tau1[beta2] = (*(_tau1[beta2]))(jet);
+  tau2[beta2] = (*(_tau2[beta2]))(jet);
+  
   // ecfgs
-  e12 = (*_1ecf2)(jet);
-  e13 = (*_1ecf3)(jet);
-  e23 = (*_2ecf3)(jet);
-  e33 = (*_3ecf3)(jet);
+  e12[beta1] = (*(_1ecf2[beta1]))(jet);
+  e13[beta1] = (*(_1ecf3[beta1]))(jet);
+  e23[beta1] = (*(_2ecf3[beta1]))(jet);
+  e33[beta1] = (*(_3ecf3[beta1]))(jet);
+  
+  e12[beta2] = (*(_1ecf2[beta2]))(jet);
+  e13[beta2] = (*(_1ecf3[beta2]))(jet);
+  e23[beta2] = (*(_2ecf3[beta2]))(jet);
+  e33[beta2] = (*(_3ecf3[beta2]))(jet);
 }
 
--- analyses/SubstructureVariables.hh
+++ analyses/SubstructureVariables.hh
@@ -37,6 +37,7 @@
 class SubstructureVariables{
 public:
   enum groom {plain, loose, tight, trim};
+  enum beta {beta1, beta2};
   /// ctor (initialises all tools)
   SubstructureVariables(CmdLine &cmdline);
 
@@ -46,32 +47,32 @@ public:
   // get the results
   double m(groom g) const{ return _m_vals[g];}
   
-  double tau1(groom g) const { return _tau1_vals[g];}
-  double tau2(groom g) const { return _tau2_vals[g];}
-  double tau21(groom g1, groom g2) const {
-    return _tau2_vals[g1]/_tau2_vals[g2];}
-  double tau21(groom g) const { return tau21(g,g);}
+  double tau1(groom g, beta b) const { return _tau1_vals[g][b];}
+  double tau2(groom g, beta b) const { return _tau2_vals[g][b];}
+  double tau21(groom g1, groom g2, beta b) const {
+    return _tau2_vals[b][g1]/_tau2_vals[b][g2];}
+  double tau21(groom g, beta b) const { return tau21(g,g,b);}
 
 
   // ecfgs
-  double ecfg_v1_N2(groom g) const {return _1ecf2_vals[g];}
-  double ecfg_v1_N3(groom g) const {return _1ecf3_vals[g];}
-  double ecfg_v2_N3(groom g) const {return _2ecf3_vals[g];}
-  double ecfg_v3_N3(groom g) const {return _3ecf3_vals[g];}
+  double ecfg_v1_N2(groom g, beta b) const {return _1ecf2_vals[g][b];}
+  double ecfg_v1_N3(groom g, beta b) const {return _1ecf3_vals[g][b];}
+  double ecfg_v2_N3(groom g, beta b) const {return _2ecf3_vals[g][b];}
+  double ecfg_v3_N3(groom g, beta b) const {return _3ecf3_vals[g][b];}
 
   // ecfs based observables
-  double U1(groom g) const {return _1ecf2_vals[g];}
-  double U2(groom g) const {return _1ecf3_vals[g];}
-  double N2(groom g1, groom g2) const {
-    return _2ecf3_vals[g1] / (_1ecf2_vals[g2]*_1ecf2_vals[g2]);}
-  double N2(groom g) const {return N2(g,g);}
-  double M2(groom g1, groom g2) const {
-    return _1ecf3_vals[g1] / _1ecf2_vals[g2];}
-  double M2(groom g) const {return M2(g,g);}
-  double D2(groom g1, groom g2) const {
-    return _3ecf3_vals[g1]
-      / (_1ecf2_vals[g2]*_1ecf2_vals[g2]*_1ecf2_vals[g2]);}
-  double D2(groom g) const { return D2(g,g);}
+  double U1(groom g, beta b) const {return _1ecf2_vals[g][b];}
+  double U2(groom g, beta b) const {return _1ecf3_vals[g][b];}
+  double N2(groom g1, groom g2, beta b) const {
+    return _2ecf3_vals[g2][b] / (_1ecf2_vals[g2][b]*_1ecf2_vals[g2][b]);}
+  double N2(groom g, beta b) const {return N2(g,g,b);}
+  double M2(groom g1, groom g2, beta b) const {
+    return _1ecf3_vals[g2][b] / _1ecf2_vals[g2][b];}
+  double M2(groom g, beta b) const {return M2(g,g,b);}
+  double D2(groom g1, groom g2, beta b) const {
+    return _3ecf3_vals[g2][b]
+      / (_1ecf2_vals[g2][b]*_1ecf2_vals[g2][b]*_1ecf2_vals[g2][b]);}
+  double D2(groom g, beta b) const { return D2(g,g,b);}
   
 protected:
   //----------------------------------------------------------------------
@@ -79,14 +80,14 @@ protected:
   double _m_vals[4];                   ///< masses
   
   // N-subjettiness
-  double _tau1_vals[3]; ///< tau1(beta)
-  double _tau2_vals[3]; ///< tau2(beta)
+  double _tau1_vals[3][2]; ///< tau1(beta=1,2)
+  double _tau2_vals[3][2]; ///< tau2(beta=1,2)
 
   // ecfgs
-  double _1ecf2_vals[3]; ///< 1ecfg2(beta)
-  double _1ecf3_vals[3]; ///< 1ecfg3(beta)
-  double _2ecf3_vals[3]; ///< 2ecfg3(beta)
-  double _3ecf3_vals[3]; ///< 3ecfg3(beta)
+  double _1ecf2_vals[3][2]; ///< 1ecfg2(beta=1,2)
+  double _1ecf3_vals[3][2]; ///< 1ecfg3(beta=1,2)
+  double _2ecf3_vals[3][2]; ///< 2ecfg3(beta=1,2)
+  double _3ecf3_vals[3][2]; ///< 3ecfg3(beta=1,2)
   
   //----------------------------------------------------------------------
   // substructure tools
@@ -97,20 +98,19 @@ protected:
   fastjet::SharedPtr<fastjet::contrib::SoftDrop> _sd;
 
   /// shape tools
-  fastjet::SharedPtr<fastjet::contrib::Nsubjettiness> _tau1, _tau2;
+  fastjet::SharedPtr<fastjet::contrib::Nsubjettiness> _tau1[2], _tau2[2];
   fastjet::SharedPtr<fastjet::contrib::EnergyCorrelatorGeneralized>
-    _1ecf2, _1ecf3, _2ecf3, _3ecf3;
+    _1ecf2[2], _1ecf3[2], _2ecf3[2], _3ecf3[2];
 
   /// underlying needs
   fastjet::SharedPtr<fastjet::Recluster> _recluster_ca;
 
   //----------------------------------------------------------------------
   // helpers
-  void _compute_one_level(const fastjet::PseudoJet &jet,
-                          double &m,
-                          double &tau1, double &tau2,
-			  double &e12, double& e13,
-			  double &e23, double& e33);
+  void _compute_one_level(const fastjet::PseudoJet &jet, 
+                          double (&tau1)[2], double (&tau2)[2],
+			  double (&e12)[2], double (&e13)[2],
+			  double (&e23)[2], double (&e33)[2]);
 
   std::ostringstream _description;
 };
