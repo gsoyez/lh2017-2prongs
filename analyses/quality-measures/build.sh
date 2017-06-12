9bb72f5576ab73970b2f7efc9258df73b9a11b7d
gsoyez <gregory.soyez@cea.fr>
2017-06-12 09:06:38 +0200

    updated quality measures using the latest N-tuples

--- analyses/quality-measures/build.sh
+++ analyses/quality-measures/build.sh
@@ -1,47 +1,57 @@
 #!/bin/bash
 #
 # create a tale with the basic quality measures
-echo "# 60<m<100, R=1, Pythia8(4C), lhc14"
-echo "#columns: combination pt  sig_truth_hadron Srob_truth_hadron Brob_truth_hadron sig_hadron_parton Srob_hadron_parton Brob_hadron_parton"
 
-# basic shapes
-for base in tau21 D2 M2 N2; do
-    # lopo over beta values
-    for beta in 1 2; do    # mass, num, den (mass<=den<=num)
-        for level in ppp lll lpl lpp ttt tlt tpt tll tpl tpp trimmed; do
-            m=${level:0:1}
-            case $m in
-                p) mass="plain"
-                   ;;
-                l) mass="loose"
-                   ;;
-                t) mass="tight"
-                   ;;
-            esac
-            lshape=${level:1:3}
+levelpairs="hadron_parton truth_hadron pu50_truth"
+Rs="0.8 1.0"
 
-            if [[ ${level} == "trimmed" ]]; then
-                m="trim"
-                lshape="trimmed"
-            fi
+for R in $Rs; do
+    file="qualities-R${R}.res"
+    echo "# lhc14, Pythia8(4C), R=${R}, 60<m<100, epsilon_S(first)=0.4" >> ${file}
+    echo -n "#columns: combination pt " >> ${file}
+    for l in $levelpairs; do
+        echo -n " significance_${l%%_*} Sresilience_${l} Bresilience_${l}" >> ${file}
+    done
+    echo ""  >> ${file}
 
-            # loop over pt values
-            for pt in 500 1000 2000; do
+    # basic shapes
+    for base in tau21 D2 M2 N2; do
+        # lopo over beta values
+        for beta in 1 2; do    # mass, num, den (mass<=den<=num)
+            for level in ppp lll lpl lpp ttt tlt tpt tll tpl tpp trimmed; do
+                m=${level:0:1}
+                case $m in
+                    p) mass="plain"
+                       ;;
+                    l) mass="loose"
+                       ;;
+                    t) mass="tight"
+                       ;;
+                esac
+                lshape=${level:1:3}
 
-                echo -n "${base}_beta${beta}_${level} ${pt}"
+                if [[ ${level} == "trimmed" ]]; then
+                    m="trim"
+                    lshape="trimmed"
+                fi
                 
-                # various comparisons
-                for level_comp in "truth:hadron" "hadron:parton"; do
-                    lref=${level_comp%:*}
-                    lalt=${level_comp#*:}
-                    q=`../scripts/get_perf_robustness.sh ../res/lhc14-WW${pt}-pythia84C-${lref}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lref}.ntuple.gz ../res/lhc14-WW${pt}-pythia84C-${lalt}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lalt}.ntuple.gz "${base}_beta${beta}_${lshape}" 0.4 "m_${mass}>60.0" "m_${mass}<100.0"`
-                    echo -n " ${q}"
-                done
-                echo ""
-            done # pt loop
-        done # grooming level
-    done # beta
-done # shapes
+                # loop over pt values
+                for pt in 500 1000 2000; do
 
-            
+                    echo -n "${base}_beta${beta}_${level} ${pt}" >> ${file}
+                
+                    # various comparisons
+                    for level_comp in $levelpairs; do
+                        lref=${level_comp%_*}
+                        lalt=${level_comp#*_}
+                        q=`../scripts/get_perf_robustness.sh ../res/lhc14-WW${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-WW${pt}-pythia84C-${lalt}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lalt}-R${R}.ntuple.gz "${base}_beta${beta}_${lshape}" 0.4 "m_${mass}>60.0" "m_${mass}<100.0"`
+                        echo -n " ${q}" >> ${file}
+                    done
+                    echo "" >> ${file}
+                done # pt loop
+            done # grooming level
+        done # beta
+    done # shapes
+
+done # R values    
 
