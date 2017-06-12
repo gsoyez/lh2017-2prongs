175c145abbdb5cda114d71adbe77d0cbfea12081
gsoyez <gregory.soyez@cea.fr>
2017-06-12 21:37:32 +0200

    updated all the tables and removed unnecessary files

--- analyses/quality-measures/build.sh
+++ analyses/quality-measures/build.sh
@@ -2,17 +2,20 @@
 #
 # create a tale with the basic quality measures
 
-levelpairs="hadron_parton truth_hadron pu50_truth"
+levelpairs="hadron_parton truth_hadron truth_parton pu50_truth ATLAS_truth CMS_truth"
 Rs="0.8 1.0"
 
 for R in $Rs; do
-    file="qualities-R${R}.res"
-    echo "# lhc14, Pythia8(4C), R=${R}, 60<m<100, epsilon_S(first)=0.4" >> ${file}
-    echo -n "#columns: combination pt " >> ${file}
+    file="qualities-R${R}.new"
+    echo "# lhc14, Pythia8(4C), R=${R}, 60<m<100, epsilon_S(first)=0.4"  | tee -a ${file}
+    echo "# significance is defined as epsS/sqrt(epsB) calculated on the reference"  | tee -a ${file}
+    echo "# correction is defined as (eps_alt-eps_ref)/eps_ref calculated either on the signal or on the background"  | tee -a ${file}
+    echo "# Note also that when 2 levels are specified, they are ordered as ref_alt"  | tee -a ${file}
+    echo -n "#columns: combination pt "  | tee -a ${file}
     for l in $levelpairs; do
-        echo -n " significance_${l%%_*} Sresilience_${l} Bresilience_${l}" >> ${file}
+        echo -n " significance_${l%%_*} Scorrection_${l} Bcorrection_${l}"  | tee -a ${file}
     done
-    echo ""  >> ${file}
+    echo ""   | tee -a ${file}
 
     # basic shapes
     for base in tau21 D2 M2 N2; do
@@ -38,16 +41,16 @@ for R in $Rs; do
                 # loop over pt values
                 for pt in 500 1000 2000; do
 
-                    echo -n "${base}_beta${beta}_${level} ${pt}" >> ${file}
+                    echo -n "${base}_beta${beta}_${level} ${pt}"  | tee -a ${file}
                 
                     # various comparisons
                     for level_comp in $levelpairs; do
                         lref=${level_comp%_*}
                         lalt=${level_comp#*_}
                         q=`../scripts/get_perf_robustness.sh ../res/lhc14-WW${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-WW${pt}-pythia84C-${lalt}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lalt}-R${R}.ntuple.gz "${base}_beta${beta}_${lshape}" 0.4 "m_${mass}>60.0" "m_${mass}<100.0"`
-                        echo -n " ${q}" >> ${file}
+                        echo -n " ${q}"  | tee -a ${file}
                     done
-                    echo "" >> ${file}
+                    echo ""  | tee -a ${file}
                 done # pt loop
             done # grooming level
         done # beta
