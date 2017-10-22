2d983f08c0f2455c28e26e2b312f82bceaa221e4
Gregory Soyez <soyez@lpthe.jussieu.fr>
2017-10-22 12:29:40 +0200

    updated the quality measures using new results and storing efficiencies rather than qiuality measures (so that one can reconstruct any quality measure we want; old results saved as backup

--- analyses/quality-measures/build.sh
+++ analyses/quality-measures/build.sh
@@ -7,13 +7,15 @@ Rs="0.8 1.0"
 
 for R in $Rs; do
     file="qualities-R${R}.new"
-    echo "# lhc14, Pythia8(4C), R=${R}, 60<m<100, epsilon_S(first)=0.4"  | tee -a ${file}
+    echo "# lhc14, Pythia8.230(Monash13), R=${R}, 65<m<105, epsilon_S(first)=0.4"  | tee -a ${file}
     echo "# significance is defined as epsS/sqrt(epsB) calculated on the reference"  | tee -a ${file}
     echo "# correction is defined as (eps_alt-eps_ref)/eps_ref calculated either on the signal or on the background"  | tee -a ${file}
     echo "# Note also that when 2 levels are specified, they are ordered as ref_alt"  | tee -a ${file}
     echo -n "#columns: combination pt "  | tee -a ${file}
     for l in $levelpairs; do
-        echo -n " significance_${l%%_*} Scorrection_${l} Bcorrection_${l}"  | tee -a ${file}
+        lref=${l%_*}
+        lalt=${l#*_}
+        echo -n " epsilon_S_${lalt}-${lref} epsilon_B_${lref} epsilon_B_${lalt}-${lref}"  | tee -a ${file}
     done
     echo ""   | tee -a ${file}
 
@@ -47,7 +49,7 @@ for R in $Rs; do
                     for level_comp in $levelpairs; do
                         lref=${level_comp%_*}
                         lalt=${level_comp#*_}
-                        q=`../scripts/get_perf_robustness.sh ../res/lhc14-WW${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lref}-R${R}.ntuple.gz ../res/lhc14-WW${pt}-pythia84C-${lalt}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia84C-${lalt}-R${R}.ntuple.gz "${base}_beta${beta}_${lshape}" 0.4 "m_${mass}>60.0" "m_${mass}<100.0"`
+                        q=`../scripts/get_efficiencies.sh ../res/lhc14-WW${pt}-pythia8230M13-${lref}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia8230M13-${lref}-R${R}.ntuple.gz ../res/lhc14-WW${pt}-pythia8230M13-${lalt}-R${R}.ntuple.gz ../res/lhc14-dijets${pt}-pythia8230M13-${lalt}-R${R}.ntuple.gz "${base}_beta${beta}_${lshape}" 0.4 "m_${mass}>65.0" "m_${mass}<105.0"`
                         echo -n " ${q}"  | tee -a ${file}
                     done
                     echo ""  | tee -a ${file}
